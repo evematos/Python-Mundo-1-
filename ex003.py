57e169d33d04056501ca819afd94a789ffc013f5
Evelin Matos <121374911+evematos@users.noreply.github.com>
2023-03-23 18:21:26 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,5 @@
+n1=int(input("digite primeiro número "))
+n2=int(input("digite segundo número "))
+soma=n1+n2
+print("A soma é: {}".format(soma))
+
---
+++ ex004.py
@@ -0,0 +1,24 @@
+t=input("Digite algo: ")
+print("O tipo primitivo desse valor é: {}".format(type(t)))
+#verificar se tem apenas números
+print("É numérico? {}".format(t.isnumeric()))
+
+#verificar se tem apenas letras E números (tendo 1 dos dois será true)
+print("É alfanumérico? {}".format(t.isalnum()))
+
+#verificar se tem apenas letras
+print("É alfabético? {}".format(t.isalpha()))
+
+#verificar se tudo está em letra maiúscula
+print("É maiúsculo? {}".format(t.isupper()))
+
+#verificar se tudo está em letra minúscula
+print("É maiúsculo? {}".format(t.islower()))
+
+#verificar se foi digitado APENAS espaço
+print("É maiúsculo? {}".format(t.isspace()))
+
+#verificar se SOMENTE 1 letra é maiúscula e o resto é minúscula
+print("É capitalizada? {}".format(t.istitle()))
+
+#Aqui objeto "t" é um objeto, todos os objetos string têm uma caracteristica, realizam uma funcionalidade e possuem atributos e métodos (islower, isupper, etc)
